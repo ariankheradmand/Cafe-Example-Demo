087f13e183a24bc1d83dd4ec7366eba3a8edc690
Arian Kheradmand <ariankheradmand@gmail.com>
2024-12-01 21:42:12 +0330

    basic layout of the items

--- components/Items.js
+++ components/Items.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 export default function Items() {
-  return (
-    <div>
-      <div>
-        <div>قهوه گرم و سرد</div>
-        <div>دمی بار</div>
-        <div>نوشیدنی های گرم</div>
-        <div>نوشیدنی های سرد</div>
+  const firstRow = ["قهوه گرم و سرد", "دمی بار", "نوشیدنی های گرم", "نوشیدنی های سرد"];
+  const secondRow = ["شیک", "فصلی", "تاپینگ"];
+
+  const renderItems = (items) => {
+    return items.map((item, index) => (
+      <div
+        key={index}
+        className="flex items-center justify-center py-2 border w-20 h-14 text-center rounded-md px-1
+        bg-black bg-opacity-20
+        hover:bg-opacity-80 hover:border-accent
+        "
+      >
+        {item}
       </div>
-      <div>
-        <div>شیک</div>
-        <div>فصلی</div>
-        <div>تاپینگ</div>
+    ));
+  };
+
+  return (
+    <div className="mt-7">
+      <div className="flex items-center justify-center gap-2 text-white text-sm">
+        {renderItems(firstRow)}
+      </div>
+      <div className="flex items-center justify-center gap-2 mt-7 text-white text-sm">
+        {renderItems(secondRow)}
       </div>
     </div>
   );
