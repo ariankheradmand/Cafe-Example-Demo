de65cdc0e5cf4c0e16fa57d0140d3c3ec5e1f65f
Arian Kheradmand <ariankheradmand@gmail.com>
2024-12-06 22:20:14 +0330

    Loading Screen V2

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react";
-
+import { BarLoader } from "react-spinners";
 export default function LoadingScreen() {
   const [progress, setProgress] = useState(0);
 
@@ -22,13 +22,12 @@ export default function LoadingScreen() {
 
   return (
     <div className="fixed inset-0 bg-accent  flex flex-col items-center justify-center z-50">
-    <h1 className="text-4xl text-black font-bold mb-4">Novo Compelex</h1>
-      <div className="w-3/4 h-3 bg-black ">
-        <div
-          className="h-full bg-white "
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
+    <h1 className="text-4xl text-black font-bold mb-4 ">NOVO COMPELEX</h1>
+    <BarLoader
+    width={250}
+    height={10}
+    className="rounded-xl"
+    />
     </div>
   );
 }
